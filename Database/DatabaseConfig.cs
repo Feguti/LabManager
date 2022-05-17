be7ce1790313fc778db156f7d026dffb14309472
Felipe Gutierrez <feguti4@gmail.com>
2022-05-17 14:44:57 -0300

    refactor: create database setup class

---
+++ Database/DatabaseConfig.cs
@@ -0,0 +1,6 @@
+namespace LabManager.Database;
+
+class DatabaseConfig
+{
+    public string ConnectionString { get => "Data Source=databse.db"; }
+}
--- Program.cs
+++ Program.cs
@@ -1,47 +1,27 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using LabManager.Database;
+using Microsoft.Data.Sqlite;
 
-var connection = new SqliteConnection("Data Source=database.db");
-connection.Open();
-
-var command = connection.CreateCommand();
-
-command.CommandText = @"
-    CREATE TABLE IF NOT EXISTS Computers(
-        id int not null primary key,
-        ram varchar(100) not null,
-        processor varchar(100) not null
-    );
-
-    CREATE TABLE IF NOT EXISTS Laboratories(
-        id int not null primary key,
-        number int not null,
-        name varchar(100) not null,
-        block var not null
-    );
-";
-command.ExecuteNonQuery();
-
-connection.Close();
+var databaseConfig = new DatabaseConfig();
+var databaseSetup = new DatabaseSetup(databaseConfig);
 
 // Routing
-
 var modelName = args[0];
 var modelAction = args[1];
 
-if(modelName == "Computer")
+if (modelName == "Computer")
 {
-    if(modelAction == "List")
+    if (modelAction == "List")
     {
-        connection = new SqliteConnection("Data Source=database.db");
+        var connection = new SqliteConnection("Data Source=database.db");
         connection.Open();
 
-        command = connection.CreateCommand();
+        var command = connection.CreateCommand();
         command.CommandText = "SELECT id, ram, processor FROM Computers;";
 
         var reader = command.ExecuteReader();
 
         Console.WriteLine("\nComputer List\n");
-        while(reader.Read())
+        while (reader.Read())
         {
             Console.WriteLine("{0}, {1}, {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
         }
@@ -50,16 +30,17 @@ if(modelName == "Computer")
         connection.Close();
 
     }
-    if(modelAction == "New")
+
+    if (modelAction == "New")
     {
         int id = Convert.ToInt32(args[2]);
         string ram = args[3];
         string processor = args[4];
 
-        connection = new SqliteConnection("Data Source=database.db");
+        var connection = new SqliteConnection("Data Source=database.db");
         connection.Open();
 
-        command = connection.CreateCommand();
+        var command = connection.CreateCommand();
         command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor)";
         command.Parameters.AddWithValue("$id", id);
         command.Parameters.AddWithValue("$ram", ram);
@@ -67,58 +48,6 @@ if(modelName == "Computer")
         command.ExecuteNonQuery();
 
         connection.Close();
-        
     }
-
-    if(modelName == "Laboratory"){
-
-    if (modelAction == "List")
-    {
-        connection = new SqliteConnection("Data Source = database.db");
-        connection.Open();
-
-        command = connection.CreateCommand();
-
-        command.CommandText = "SELECT * FROM Laboratories";
-
-        var reader = command.ExecuteReader();
-
-        while (reader.Read())
-        {
-            Console.WriteLine(
-                "{0},{1},{2},{3}",reader.GetInt32(0),reader.GetInt32(1),reader.GetString(2),reader.GetChar(3)
-            );
-        }
-
-        reader.Close();
-        connection.Close();
-    }
-
-    if (modelAction == "New")
-    {
-        var id = Convert.ToInt32(args[2]);
-        var number = Convert.ToInt32(args[3]);
-        var name = args[4];
-        var block = args[5];
-
-        connection = new SqliteConnection("Data Source = database.db");
-
-        connection.Open();
-
-        command = connection.CreateCommand();
-
-        command.CommandText = @"
-            INSERT INTO Laboratories VALUES($id, $number, $name, $block);
-        ";
-        command.Parameters.AddWithValue("$id",id);
-        command.Parameters.AddWithValue("$number",number);
-        command.Parameters.AddWithValue("$name",name);
-        command.Parameters.AddWithValue("$block",block);
-
-        command.ExecuteNonQuery();
-
-        connection.Close();
-    }
-}
 
 }
